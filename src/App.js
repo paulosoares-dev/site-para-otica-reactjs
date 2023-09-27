a13160bf66bdfb550cd6b402446d39b7b59c8c8f
Paulo Soares <contato@ptek.com.br>
2023-09-27 04:38:52 -0300

    Estrutura base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+import Topo from './componentes/Topo';
+import Conteudo from './componentes/Conteudo';
+import Rodape from './componentes/Rodape';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App container">
+            <Topo />
+            <Conteudo />
+            <Rodape />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/componentes/Conteudo/index.js
@@ -0,0 +1,15 @@
+import SecaoCapa from "../SecaoCapa";
+import SecaoProdutos from "../SecaoProdutos";
+import SecaoSobre from "../SecaoSobre";
+import SecaoContato from "../SecaoContato";
+
+export default function Conteudo() {
+    return (
+        <main>
+            <SecaoCapa />
+            <SecaoProdutos />
+            <SecaoSobre />
+            <SecaoContato />
+        </main>
+    )
+}
